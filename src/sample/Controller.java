4beaa126158f58243e0ade0106c0fef241462d97
SuperRsDev <muharemov1@etf.unsa.ba>
2019-04-10 09:16:58 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public TextField unosImena;
+    public TextField unosPrezimena;
+    public TextField unosEmaila;
+    public TextField unosKorisnickogImena;
+    public TextField unosLozinke;
+}
